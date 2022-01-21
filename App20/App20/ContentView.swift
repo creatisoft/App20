83de0b7533c33b12779207dc3890feac4e617ca2
Christopher M <chrism1389@gmail.com>
2022-01-20 21:24:58 -0800

    Add files via upload

---
+++ App20/App20/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  App20
+//
+//  Created by Christopher on 1/20/22.
+//  https://www.creatisoft.com
+// this is view #1
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+    
+        VStack{
+            
+            ViewOne()
+                .padding(.all)
+                
+        }.frame(width: 300, height: 250, alignment: .center)
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+    
+        ContentView()
+   
+    }
+}
